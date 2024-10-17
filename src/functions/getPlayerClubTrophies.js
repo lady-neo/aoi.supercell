e3eae6b55c0d2665f56e5814b46f9da1afe48c2d
just_neo_ <thaviny.to.use.games@gmail.com>
2024-10-17 15:09:21 -0300

    Update getPlayerClubTrophies.js

--- src/functions/getPlayerClubTrophies.js
+++ src/functions/getPlayerClubTrophies.js
@@ -102,7 +102,7 @@ module.exports = {
 
       return {
         code: d.util.setCode(data)
-      };
+      }
     } catch (error) {
       return d.aoiError.fnError(d, "custom", {}, "Failed to retrieve player club trophies. Please check the ID and token.");
     }
