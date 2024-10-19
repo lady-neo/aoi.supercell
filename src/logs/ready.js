657aecf416c9b79a82863ef4539b915686bb4966
just_neo_ <thaviny.to.use.games@gmail.com>
2024-10-19 11:40:48 -0300

    Create ready.js

---
+++ src/logs/ready.js
@@ -0,0 +1,24 @@
+const { AoiError } = require("aoi.js");
+const { version } = require("../../package.json");
+
+const installed = () => {
+  AoiError.createConsoleMessage([{ text: `Installed on v${version}`, textColor: "green" }], borderColor = "white", { text: "aoi.supercell", textColor: "cyan" });
+};
+
+const tokenManagerInfo = (context) => {
+  const messages = [
+    { text: "Token Registration Method:", textColor: "white" },
+    { text: `${context.registerTokenMethod}`, textColor: "yellow" },
+  ];
+
+  if (context.registerTokenMethod === "file") {
+    messages.push(
+      { text: "Token Registration Path:", textColor: "white" },
+      { text: `${context.tokenPath}`, textColor: "yellow" }
+    );
+  }
+
+  AoiError.createConsoleMessage(messages, "white", { text: "Token Manager Info", textColor: "cyan" });
+};
+
+module.exports = { installed, tokenManagerInfo };
